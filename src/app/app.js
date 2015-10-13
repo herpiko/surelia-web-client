cda533945113ba0d756c7951c3d8c8d4f59dee70
Herpiko Dwi Aguno <herpiko@gmail.com>
2015-10-14 05:08:38 +0700

    IMAP service and simple UI to test them

--- src/app/app.js
+++ src/app/app.js
@@ -6,23 +6,26 @@ angular.module("App", [
   "test",
   "html",
   "angularFileUpload",
-  "start"
+  "start",
+  "imapService",
+  "LocalStorageModule"
 ])
 .config(function($stateProvider) {
   $stateProvider
   .state("start", {
       url: "/start",
-      controller: "StartCtrl",
       templateProvider: function($templateCache) {
         return $templateCache.get("start/start.html");
       }
     }
   )
 })
-.controller("AppCtrl", function($scope) {
+.controller("AppCtrl", function($scope, $state) {
+    $state.go("start");
 })
 .run([ "$rootScope", "$state", "$stateParams", 
   function ($rootScope, $state, $stateParams) {
+    
   }
 ])
 
