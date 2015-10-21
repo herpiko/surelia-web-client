98e48cd68f427a52459f139bbb8aaa9afcefbda5
Herpiko Dwi Aguno <herpiko@gmail.com>
2015-10-21 14:22:17 +0700

    Remove console.log

--- gulp/tasks/db.js
+++ gulp/tasks/db.js
@@ -8,9 +8,8 @@ var browserSync  = require('browser-sync');
 
 gulp.task('db', function () {
   var db = process.env.DB || "test";
-  var conf = gulp.src(config.db.src)
-  console.log(conf);
-    conf.pipe(replace("test", db))
+  gulp.src(config.db.src)
+    .pipe(replace("test", db))
     .pipe(gulp.dest(config.db.dest))
     .pipe(gulpif(browserSync.active, browserSync.reload({ stream: true, once: true })));
 });
