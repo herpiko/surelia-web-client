2c0613a099c79ed010ca6aad7a059544bfa8e552
Herpiko Dwi Aguno <herpiko@gmail.com>
2015-11-09 12:46:53 +0700

    Email validation in front-end

--- src/app/service/toastrService.js
+++ src/app/service/toastrService.js
@@ -13,6 +13,20 @@ ToastrService.prototype.invalidCredentials = function(){
   self.toastr.error("Invalid credentials");
 }
 
+ToastrService.prototype.invalidEmailAddress = function(emailString){
+  var self = this;
+  if (emailString) {
+    self.toastr.error(emailString + " is not a valid email address");
+  } else {
+    self.toastr.error("Invalid email address");
+  }
+}
+
+ToastrService.prototype.emptyRecipients = function(){
+  var self = this;
+  self.toastr.error("Recipients should not be empty");
+}
+
 ToastrService.prototype.sent = function() {
   var self = this;
   self.toastr.success("Your message was sent successfully");
