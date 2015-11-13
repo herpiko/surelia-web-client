e0180a5658c27bac32906b7378c095793a43f192
Herpiko Dwi Aguno <herpiko@gmail.com>
2015-11-14 02:34:04 +0700

    Fixes 1000 separator in quota

--- src/app/filters/size.js
+++ src/app/filters/size.js
@@ -2,9 +2,9 @@
 var SizeFilter = function() {
   return function(bytes) {
     if(bytes < 1024) return bytes + " Bytes";
-    else if(bytes < 1048576) return(bytes / 1024).toFixed(3) + " KB";
-    else if(bytes < 1073741824) return(bytes / 1048576).toFixed(3) + " MB";
-    else return(bytes / 1073741824).toFixed(3) + " GB";
+    else if(bytes < 1048576) return parseFloat((bytes / 1024).toFixed(3)).toLocaleString() + " KB";
+    else if(bytes < 1073741824) return parseFloat((bytes / 1048576).toFixed(3)).toLocaleString() + " MB";
+    else return parseFloat((bytes / 1073741824).toFixed(3)).toLocaleString() + " GB";
   }
 }
 
