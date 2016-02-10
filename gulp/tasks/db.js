7d4d55ddc9185fb9f0e4f3b632c3edc0c0eefc4c
Herpiko Dwi Aguno <herpiko@gmail.com>
2016-02-10 17:52:51 +0700

    Add optional DB_HOST env for database configuration

--- gulp/tasks/db.js
+++ gulp/tasks/db.js
@@ -8,8 +8,10 @@ var browserSync  = require('browser-sync');
 
 gulp.task('db', function () {
   var db = process.env.DB || "test";
+  var host = process.env.DB_HOST || "localhost";
   gulp.src(config.db.src)
     .pipe(replace("test", db))
+    .pipe(replace("localhost", host))
     .pipe(gulp.dest(config.db.dest))
     .pipe(gulpif(browserSync.active, browserSync.reload({ stream: true, once: true })));
 });
